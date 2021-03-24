32ca057c732295faabb706df6a266934280a3ae9
Rio Surya Fachrudin <32932036+riosurya12@users.noreply.github.com>
2021-03-24 09:02:37 +0700

    Add files via upload

---
+++ Tugas 2 PBKK C/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tugas_2_PBKK_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("Rio Surya Fachrudin");
+            Console.WriteLine("05111640000130");
+            Console.WriteLine("Tugas 2 PBKK C");
+            Console.WriteLine();
+            Console.WriteLine("Daftar Aplikasi yang pernah dibangun:");
+            Console.WriteLine("++ Pengelolaan Barang Gudang (SIERP)  (Menggunakan Laravel dan Boostrap) ");
+            Console.WriteLine("++ Website Pribadi (Menggunakan Bootstrap dan CSS) ");
+            Console.WriteLine("++ Beberapa tugas dari perkuliahan (Menggunakan Java) ");
+            Console.WriteLine("");
+            Console.WriteLine("Terima kasih :)) ");
+
+        }
+    }
+}
